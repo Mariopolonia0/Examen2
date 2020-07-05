1a03a8ccb553f97bb797ae61d95f7e364f28492d
Mario <Mario@DESKTOP-JLRVOOA>
2020-07-05 18:41:57 -0400

    hola

---
+++ Examen1/DAL/Contexto.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Examen1.DAL
+{
+    public class Contexto : DbContext
+    {
+       
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite(@"Data Source= DATA\TeacherControl.db");
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Nacionalidades>().HasData(new Nacionalidades
+            {
+                NacionalidadId = 1,
+                Nacionalidad = "Dominicana"
+            });
+        }
+    }
+}
